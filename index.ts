409babc4f226998d677cb5b0658b9bba79eac428
thuyphuocthinh <104757603+thuyphuocthinh@users.noreply.github.com>
2024-08-11 11:59:24 +0700

    updated appolo server

--- index.ts
+++ index.ts
@@ -20,6 +20,7 @@ const startServer = async () => {
   const apolloServer = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers,
+    introspection: true,
     context: ({ req }) => {
       return { ...req };
     },
